b8d202fceb1365a6d447631f2278cb8aef0e666f
jeffngugi <ngugimwathi254@gmail.com>
2019-08-25 23:29:54 +0300

    search log functionality

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -6,7 +6,8 @@ import {
   DELETE_LOG,
   UPDATE_LOG,
   CLEAR_CURRENT,
-  SET_CURRENT
+  SET_CURRENT,
+  SEARCH_LOGS
 } from './types';
 
 // export const getLogs = () =>{
@@ -78,6 +79,26 @@ export const updateLog = (log) => async (dispatch) => {
     const data = await res.json();
     dispatch({
       type: UPDATE_LOG,
+      payload: data
+    });
+  } catch (err) {
+    dispatch({
+      type: LOGS_ERROR,
+      payload: err.response.data
+    });
+  }
+};
+
+//search logs
+export const searchLogs = (text) => async (dispatch) => {
+  try {
+    setLoading();
+
+    const res = await fetch(`/logs?q=${text}`);
+    const data = await res.json();
+
+    dispatch({
+      type: SEARCH_LOGS,
       payload: data
     });
   } catch (err) {
